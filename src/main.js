2c29255e8e10a2909eb57a8fd3e198798ae53966
Martin Bohal <mb@mbohal.cz>
2022-12-13 14:43:00 +0100

    Add `<docoff-placeholder>` component

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { DocoffPlaceholder } from './DocoffPlaceholder';
 import { DocoffReactPreview } from './DocoffReactPreview';
 import { DocoffReactBase } from './DocoffReactBase';
 import { DocoffReactProps } from './DocoffReactProps';
@@ -5,3 +6,4 @@ import { DocoffReactProps } from './DocoffReactProps';
 customElements.define('docoff-react-preview', DocoffReactPreview, { extends: 'textarea' });
 customElements.define('docoff-react-base', DocoffReactBase, { extends: 'textarea' });
 customElements.define('docoff-react-props', DocoffReactProps);
+customElements.define('docoff-placeholder', DocoffPlaceholder);
